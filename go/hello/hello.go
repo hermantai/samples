3a3ec621d7f3d894c572b3217600a97210aecd81
Herman Tai <htaihm@gmail.com>
2015-09-03 06:31:08 -0700

    [go]Add reverse.go
    Still working on https://golang.org/doc/code.html ...

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/hermantai/samples/go/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello world!\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
