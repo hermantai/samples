00fd2d131f89b68df30583f4df77138e9f2e58ab
Herman Tai <htaihm@gmail.com>
2015-11-29 14:24:34 -0800

    fix the indentation of simple_no_logging_script_template.py

--- python/simple_no_logging_script_template.py
+++ python/simple_no_logging_script_template.py
@@ -4,19 +4,19 @@ of the command line arguments through the function parse_args, the whole
 application control goes to the class App.
 
 
-   Copyright 2015 Herman Tai
+    Copyright 2015 Herman Tai
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 from __future__ import print_function
 from __future__ import absolute_import
