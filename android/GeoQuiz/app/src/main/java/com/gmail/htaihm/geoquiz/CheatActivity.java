ddfdff4ea3633d16a6fbc2cbb7b627d5b4254a86
Herman Tai <htaihm@gmail.com>
2016-01-19 01:09:32 -0800

    [android][GeoQuiz] Finished up the GeoQuiz with the finishing of Chapter 6 of Android Programming

--- android/GeoQuiz/app/src/main/java/com/gmail/htaihm/geoquiz/CheatActivity.java
+++ android/GeoQuiz/app/src/main/java/com/gmail/htaihm/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.gmail.htaihm.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -34,7 +38,16 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
-        mAnswerTextView = (TextView) findViewById(R.id.answer_text_field);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        TextView apiVersionTextView = (TextView) findViewById(R.id.api_version_text_view);
+        apiVersionTextView.setText(
+                "API level "
+                        + Build.VERSION.SDK_INT
+                        + ", "
+                        + Build.VERSION.CODENAME
+                        + ", "
+                        + Build.VERSION.RELEASE);
 
         mShowAnswerButton = (Button) findViewById((R.id.show_answer_button));
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +59,23 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    final Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
