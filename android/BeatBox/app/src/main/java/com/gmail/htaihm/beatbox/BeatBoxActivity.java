39b14ac4943a9c584b598799b722b94e961502bc
Herman Tai <htaihm@gmail.com>
2016-01-24 22:18:07 -0800

    [android][BeatBox] Initial version of BeatBox, finished "Android Programming" Chapter 18 Assets

---
+++ android/BeatBox/app/src/main/java/com/gmail/htaihm/beatbox/BeatBoxActivity.java
@@ -0,0 +1,11 @@
+package com.gmail.htaihm.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
