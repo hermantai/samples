31b654382fc0457a2d50602302d156b0b8a05154
Herman Tai <htaihm@gmail.com>
2016-01-30 22:29:03 -0800

    [android][PhotoGallery] Finished "Android Programming" Ch 26. Background Services

--- android/PhotoGallery/app/src/main/java/com/gmail/htaihm/photogallery/PhotoGalleryActivity.java
+++ android/PhotoGallery/app/src/main/java/com/gmail/htaihm/photogallery/PhotoGalleryActivity.java
@@ -1,8 +1,13 @@
 package com.gmail.htaihm.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
