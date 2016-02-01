2f8a60742edf851457ccc10cbea4d39e0b3c65a3
Herman Tai <htaihm@gmail.com>
2016-01-31 21:24:34 -0800

    [android][Sunset] Finished "Android Programming" Ch 30. Property Animation

---
+++ android/Sunset/app/src/main/java/com/gmail/htaihm/sunset/SunsetActivity.java
@@ -0,0 +1,11 @@
+package com.gmail.htaihm.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
---
+++ android/Sunset/app/src/main/java/com/gmail/htaihm/sunset/SunsetFragment.java
@@ -0,0 +1,90 @@
+package com.gmail.htaihm.sunset;
+
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+
+public class SunsetFragment extends Fragment {
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = v;
+        mSunView = v.findViewById(R.id.sun);
+        mSkyView = v.findViewById(R.id.sky);
+
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
+        return v;
+    }
+
+    private void startAnimation() {
+        // getTop, etc, gives the location of the view in its parent.
+        // These view values are reset everytime a layout pass occurs, so they do not hold on to
+        // your modified values.
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+        // There are a lot of TimeInterpolators to change how the animation is done. Check out the
+        // subclasses of TimeInterpolators.
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        // ArgbEvaluator understands color and find the values between two colors.
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        ObjectAnimator nightSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
+                .setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        // Use AnimatorSet does not need the following. Without AnimatorSet, we have to use
+        // listeners and other stuff, which is hard.
+        //
+        // heightAnimator.start();
+        // sunsetSkyAnimator.start();
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet
+                .play(heightAnimator)
+                .with(sunsetSkyAnimator)
+                .before(nightSkyAnimator);
+        animatorSet.start();
+    }
+}
