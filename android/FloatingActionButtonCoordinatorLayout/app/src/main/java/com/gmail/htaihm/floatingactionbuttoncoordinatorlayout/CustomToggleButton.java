0e253c3291d0176288585f69321dca0e08094b94
Herman Tai <htaihm@gmail.com>
2018-11-03 09:48:56 -0700

    [android][FloatingActionButtonCoordinatorLayout] Use AttachBehavior
    DefaultBehavior is deprecated. Use AttachBehavior like: https://medium.com/@zoha131/coordinatorlayout-behavior-basic-fd9c10d3c6e3

--- android/FloatingActionButtonCoordinatorLayout/app/src/main/java/com/gmail/htaihm/floatingactionbuttoncoordinatorlayout/CustomToggleButton.java
+++ android/FloatingActionButtonCoordinatorLayout/app/src/main/java/com/gmail/htaihm/floatingactionbuttoncoordinatorlayout/CustomToggleButton.java
@@ -2,19 +2,29 @@ package com.gmail.htaihm.floatingactionbuttoncoordinatorlayout;
 
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
 import android.widget.ToggleButton;
 
-@CoordinatorLayout.DefaultBehavior(CustomToggleButtonBehavior.class)
-public class CustomToggleButton extends ToggleButton {
-    public CustomToggleButton(Context context) { super(context); }
+public class CustomToggleButton extends ToggleButton implements CoordinatorLayout.AttachedBehavior {
+  public CustomToggleButton(Context context) {
+    super(context);
+  }
 
-    public CustomToggleButton(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+  public CustomToggleButton(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
 
-    public CustomToggleButton(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+  public CustomToggleButton(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @NonNull
+  @Override
+  public CoordinatorLayout.Behavior getBehavior() {
+    // this is the preferred way to attach a behavior to a view if we want to attach it automatically
+    // https://medium.com/@zoha131/coordinatorlayout-behavior-basic-fd9c10d3c6e3
+    return new CustomToggleButtonBehavior();
+  }
 }
