dbab4bc4c661b1ec232288ec75674b57b5fe55e1
Herman Tai <htaihm@gmail.com>
2018-11-11 13:29:18 -0800

    [udacity-kotlin][DiceRoller] completed project for lesson 1
    Came from Developing Android Apps With Kotlin:
    https://www.udacity.com/course/developing-android-apps-with-kotlin--ud9012

---
+++ android/udacity-kotlin/DiceRoller/app/src/androidTest/java/com/gmail/htaihm/diceroller/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.gmail.htaihm.diceroller
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.gmail.htaihm.diceroller", appContext.packageName)
+    }
+}
