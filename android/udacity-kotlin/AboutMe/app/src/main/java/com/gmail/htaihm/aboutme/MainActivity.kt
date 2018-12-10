eeb637fed8de548df07e54ac63e782aad43a96ba
Herman Tai <htaihm@gmail.com>
2018-12-09 16:44:43 -0800

    Finish the AboutMe program for kotlin android programming

--- android/udacity-kotlin/AboutMe/app/src/main/java/com/gmail/htaihm/aboutme/MainActivity.kt
+++ android/udacity-kotlin/AboutMe/app/src/main/java/com/gmail/htaihm/aboutme/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.gmail.htaihm.aboutme
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.gmail.htaihm.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    // Instance of MyName data class.
+    private val myName: MyName = MyName("Herman Tai")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ android/udacity-kotlin/AboutMe/app/src/main/java/com/gmail/htaihm/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.gmail.htaihm.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
