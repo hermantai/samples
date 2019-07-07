eae4dbeb2a4bbdc14e24c461b45ed1cc86cbb48d
Herman Tai <htaihm@gmail.com>
2019-07-07 13:39:50 -0700

    LottieInL and FillScrollView for MyPlaygroundInJava

---
+++ android/MyPlaygroundInJava/app/src/main/java/com/gmail/htaihm/myplaygroundinjava/fillscrollview/FillScrollViewActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.htaihm.myplaygroundinjava.fillscrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FillScrollViewActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_fill_scroll_view);
+  }
+}
