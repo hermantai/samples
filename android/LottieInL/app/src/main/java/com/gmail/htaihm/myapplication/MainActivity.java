ff536e8369840a1dbe62401e01d346bbf0884215
Herman Tai <htaihm@gmail.com>
2019-07-09 15:38:11 -0700

    Add an example for remote Lottie animation

--- android/LottieInL/app/src/main/java/com/gmail/htaihm/myapplication/MainActivity.java
+++ android/LottieInL/app/src/main/java/com/gmail/htaihm/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gmail.htaihm.myapplication;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    String url = "http://www.gs"
+        + "tatic.co"
+        + "m/play/points/i"
+        + "cons/level"
+        + "_badges/lot"
+        + "tie_le"
+        + "vel2.json";
+    Log.i("MainActivity", "url is " + url);
+    LottieAnimationView lottieAnimationView = findViewById(R.id.animation_view);
+
+    lottieAnimationView.setAnimationFromUrl(url);
   }
 }
