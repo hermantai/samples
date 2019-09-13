2086f2580aec3a3993b864ff6fbdaf24215ee14f
Herman Tai <htaihm@gmail.com>
2019-09-12 23:14:57 -0700

    [go][tour] Finished "switch"

--- go/tour/hello/hello.go
+++ go/tour/hello/hello.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"math/cmplx"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -100,6 +101,32 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+
+	// while loop
+	sum := 0
+	for sum < 10 {
+		fmt.Println("Sum is", sum)
+		sum++
+	}
+
+	printSection("If")
+	fmt.Println(sqrt(2), sqrt(-4))
+
+	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+
+	printSection("exercise with loops")
+	fmt.Println(mySqrt(2))
+
+	printSection("switch")
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
 }
 
 // end of main
@@ -190,4 +217,31 @@ func constantsDependsOnContext() {
 func needInt(x int) int { return x*10 + 1 }
 func needFloat(x float64) float64 {
 	return x * 0.1
+}
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+// if with a short statement
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		// v is only in scope until the end of if/else
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
+
+func mySqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		fmt.Println(z)
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
 }
