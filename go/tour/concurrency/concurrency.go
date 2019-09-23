e5d81e4770082aa1a872a6284b3bb3bb2bd78b7b
Herman Tai <htaihm@gmail.com>
2019-09-22 22:50:01 -0700

    [go][tour] Finished up to "Equivalent Binary Trees"

--- go/tour/concurrency/concurrency.go
+++ go/tour/concurrency/concurrency.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/hermantai/samples/go/commonutil"
+	"golang.org/x/tour/tree"
 	"time"
 )
 
@@ -12,6 +13,10 @@ func main() {
 
 	commonutil.PrintSection("Channels")
 	channelsSample()
+
+	commonutil.PrintSection("Equivalent binary tree")
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
 
 // end of main
@@ -38,6 +43,45 @@ func channelsSample() {
 	x, y := <-c, <-c
 
 	fmt.Println(x, y, x+y)
+
+	commonutil.PrintSubsection("Buffered channels")
+	ch := make(chan int, 2)
+	ch <- 3
+	ch <- 4
+	fmt.Println(<-ch, <-ch)
+
+	ch2 := make(chan int, 10)
+	go fibonacci(cap(ch2), ch2)
+	for i := range ch2 {
+		fmt.Println(i)
+	}
+
+	commonutil.PrintSubsection("Select")
+	ch3 := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch3)
+		}
+		quit <- 0
+	}()
+	fibWithSelect(ch3, quit)
+
+	commonutil.PrintSubsection("default selection")
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
 
 func sum(s []int, c chan int) {
@@ -46,6 +90,65 @@ func sum(s []int, c chan int) {
 		sum += v
 	}
 	c <- sum
+}
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func fibWithSelect(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
+
+	for {
+		x, ok := <-ch1
+		if !ok {
+			_, ok := <-ch2
+			return !ok
+		}
+
+		if y, ok := <-ch2; !ok || x != y {
+			return false
+		}
+	}
 }
 
 // end of samples
