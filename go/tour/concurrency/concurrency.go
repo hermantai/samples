7d39dfb434fc43d468adced5b6babd9212e31aa6
Herman Tai <htaihm@gmail.com>
2019-09-25 17:28:46 -0700

    [go][tour] Finished "Web Crawler"

--- go/tour/concurrency/concurrency.go
+++ go/tour/concurrency/concurrency.go
@@ -21,6 +21,9 @@ func main() {
 
 	commonutil.PrintSection("Locks")
 	locksSample()
+
+	commonutil.PrintSection("exercise: web crawler")
+	webcrawlerSample()
 }
 
 // end of main
@@ -183,5 +186,99 @@ func (c *SafeCounter) Value(key string) int {
 	defer c.mux.Unlock()
 	return c.v[key]
 }
+
+func webcrawlerSample() {
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	fetchedUrlsLock.Lock()
+	if _, fetched := attemptedUrls[url]; fetched {
+		fetchedUrlsLock.Unlock()
+		return
+	}
+	attemptedUrls[url] = struct{}{}
+	fetchedUrlsLock.Unlock()
+
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	var wg sync.WaitGroup
+	for _, u := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			Crawl(url, depth-1, fetcher)
+		}(u)
+	}
+	wg.Wait()
+	return
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
+
+var attemptedUrls = make(map[string]interface{})
+var fetchedUrlsLock sync.Mutex
 
 // end of samples
