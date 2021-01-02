86f1f7fa7a83bfeec0ef94e8b25ecc18dbfaa94b
Herman Tai <htaihm@gmail.com>
2021-01-02 09:16:10 -0800

    [kotlin][developer-android/HelloWorld] Finished rolling dice
    Finished https://codelabs.developers.google.com/codelabs/kotlin-android-training-images-compat/index.html?index=..%2F..android-kotlin-fundamentals#0

--- kotlin/developer-android/HelloWorld/app/src/main/java/com/example/android/helloworld/MainActivity.kt
+++ kotlin/developer-android/HelloWorld/app/src/main/java/com/example/android/helloworld/MainActivity.kt
@@ -3,24 +3,40 @@ package com.example.android.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+  // var diceImage : ImageView? = null
+  lateinit var diceImage : ImageView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    diceImage = findViewById(R.id.dice_image)
+
     val rollButton: Button = findViewById(R.id.roll_button)
     rollButton.setOnClickListener { rollDice() }
-
-
   }
 
   private fun rollDice() {
 //    Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-    val resultText: TextView = findViewById(R.id.result_text)
+
+//    val resultText: TextView = findViewById(R.id.result_text)
     val randomInt = (1..6).random()
-    resultText.text = randomInt.toString()
+//    resultText.text = randomInt.toString()
+
+    val drawableResource = when (randomInt) {
+      1 -> R.drawable.dice_1
+      2 -> R.drawable.dice_2
+      3 -> R.drawable.dice_3
+      4 -> R.drawable.dice_4
+      5 -> R.drawable.dice_5
+      else -> R.drawable.dice_6
+    }
+    diceImage.setImageResource(drawableResource)
   }
 }
