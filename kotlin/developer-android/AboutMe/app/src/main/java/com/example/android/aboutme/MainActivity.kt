4af56257f6d505180255aa984f8e02f95b7d97a0
Herman Tai <htaihm@gmail.com>
2021-01-03 23:49:40 -0800

    [kotlin][developer-android/AboutMe] Use data binding for AboutMe
    https://developer.android.com/codelabs/kotlin-android-training-data-binding-basics/index.html#0

--- kotlin/developer-android/AboutMe/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ kotlin/developer-android/AboutMe/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -8,53 +8,57 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+  private val initialMyName: MyName = MyName("Herman 2")
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    binding.myName = initialMyName
 
-    val doneButton: Button = findViewById(R.id.done_button)
-    doneButton.setOnClickListener{ addNickname(it) }
+    binding.doneButton.setOnClickListener { addNickname() }
 
-    val nicknameText: TextView = findViewById(R.id.nickname_text)
-    nicknameText.setOnClickListener{
-      updateNickname(it)
+    binding.nicknameText.setOnClickListener {
+      updateNickname()
     }
   }
 
-  private fun addNickname(view: View) {
-    val nicknameText: TextView = findViewById(R.id.nickname_text)
-    val nicknameEdit: EditText = findViewById(R.id.nickname_edit)
+  private fun addNickname() {
+    binding.apply {
+      if (nicknameEdit.text.isEmpty()) {
+        Snackbar.make(doneButton, "The nickname cannot be empty", Snackbar.LENGTH_SHORT).show()
+        return
+      }
 
-    if (nicknameEdit.text.isEmpty()) {
-      Snackbar.make(view, "The nickname cannot be empty", Snackbar.LENGTH_SHORT).show()
-      return
+      // remove styling from EditText (e.g. underline for typos) when setting to nicknameText.text
+      //   nicknameText.text = nicknameEdit.text.toString()
+      myName?.nickname = nicknameEdit.text.toString()
+      invalidateAll()
+
+      nicknameEdit.visibility = View.GONE
+      doneButton.visibility = View.GONE
+      nicknameText.visibility = View.VISIBLE
+
+      val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      inputMethodManager.hideSoftInputFromWindow(doneButton.windowToken, 0)
     }
-
-    // remove styling from EditText (e.g. underline for typos) when setting to nicknameText.text
-    nicknameText.text = nicknameEdit.text.toString()
-    nicknameEdit.visibility = View.GONE
-    view.visibility = View.GONE
-    nicknameText.visibility = View.VISIBLE
-
-    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
   }
 
-  private fun updateNickname(view: View) {
-    val nicknameText: TextView = findViewById(R.id.nickname_text)
-    val nicknameEdit: EditText = findViewById(R.id.nickname_edit)
-    val doneButton: Button = findViewById(R.id.done_button)
+  private fun updateNickname() {
+    binding.apply {
+      nicknameEdit.setText("")
+      nicknameEdit.visibility = View.VISIBLE
+      doneButton.visibility = View.VISIBLE
+      nicknameText.visibility = View.GONE
 
-    nicknameEdit.setText("")
-    nicknameEdit.visibility = View.VISIBLE
-    doneButton.visibility = View.VISIBLE
-    nicknameText.visibility = View.GONE
-
-    nicknameEdit.requestFocus()
-    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.showSoftInput(nicknameEdit, 0)
+      nicknameEdit.requestFocus()
+      val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      imm.showSoftInput(nicknameEdit, 0)
+    }
   }
 }
