256d279e79440ee3277dc7f0e5619441eeaba5fa
Herman Tai <htaihm@gmail.com>
2021-01-10 22:04:11 -0800

    [kotlin][developer-android/AndroidTrivia-Starter] Add navigation
    Add the navigation xml graph, then add an action to connect the title
    fragment and the game fragment.
    
    https://developer.android.com/codelabs/kotlin-android-training-add-navigation/index.html#4

--- kotlin/developer-android/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ kotlin/developer-android/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -25,6 +26,9 @@ class TitleFragment : Fragment() {
       container,
       false
     )
+    binding.playButton.setOnClickListener{
+      view: View -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+    }
     return binding.root
   }
 }
