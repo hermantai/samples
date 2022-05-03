6d7f6b2c2c9a7f290ee0c86ff418b6eb11618e5e
Herman Tai <htaihm@gmail.com>
2022-05-02 23:17:30 -0700

    [rust-playground] Finished The Book 3.2 Data Types
    https://doc.rust-lang.org/book/ch03-02-data-types.html

--- rust/rust-playground/README.md
+++ rust/rust-playground/README.md
@@ -3,18 +3,24 @@ This program is created by `cargo new rust-playground`.
 
 This Playground comes from reading "The Book": https://doc.rust-lang.org/book
 
-Bookmark: https://doc.rust-lang.org/book/ch03-02-data-types.html (remember to read Chapter 2 after this)
+Bookmark: https://doc.rust-lang.org/book/ch03-03-how-functions-work.html (remember to read Chapter 2 after this)
 
 ## Dev
 Commands are run the top-level directory, i.e. "rust-playground".
 
 Use `cargo doc` to generate docs in the _target_ directory.
-The document can be viewed by opening _rust-playground/target/doc/rust_playground/index.html_.
+The document can be viewed by opening
+_rust-playground/target/doc/rust_playground/index.html_.
 
 Use `cargo build` to build the program. The program is stored as
 _target/debug/rust-playground_.
 
 Use `cargo run` to build and run the program in one command.
+
+* `cargo run input_array_index` to run the function that gets an array index
+  from stdin to print out something.
+
+Use `cargo fmt` to format the files for this module.
 
 `cargo check` is to make sure your code compiles but it doesn't create an
 executable.
--- rust/rust-playground/src/main.rs
+++ rust/rust-playground/src/main.rs
@@ -2,11 +2,17 @@
 //! Use `cargo doc` in the top-level directory to generate docs in the _target_
 //! directory.
 
+use std::env;
+use std::io;
+use std::process;
+
 // This is the main function
 /// Generate library docs for the following item. Which is a fancy main
 /// function! Support <b>HTML</b>.
 fn main() {
     // Statements here are executed when the compiled binary is called
+    print_header();
+    play_process_input();
 
     print_header();
     play_print_stuff();
@@ -16,6 +22,39 @@ fn main() {
 
     print_header();
     play_variable_stuff();
+
+    print_header();
+    play_data_types();
+}
+
+fn play_process_input() {
+    let args: Vec<String> = env::args().collect();
+    println!("Input parameters: {:?}, len = {}", args, args.len());
+
+    // args[0] is the script itself, so the first parameter is at args[1].
+    if args.len() >= 2 {
+        let operation = &args[1];
+        if operation == "input_array_index" {
+            let ar = [1, 2, 3, 4, 5];
+            println!("array is {:?}. Please enter an index:", ar);
+            let mut index = String::new();
+            io::stdin()
+                .read_line(&mut index)
+                .expect("Failed to read line");
+
+            let index: usize = index
+                .trim()
+                .parse()
+                .expect("Index entered was not a number");
+            let element = ar[index];
+            println!(
+                "The value of the element at index {} is : {}",
+                index, element
+            );
+
+            process::exit(0);
+        }
+    }
 }
 
 /**
@@ -52,9 +91,8 @@ fn play_print_stuff() {
     // You can pad numbers with extra zeroes. This will output "000001".
     println!("{number:0>width$}", number = 1, width = 6);
 
-    //
-    // Examples of named arguments:
-    //
+    let ar = [1, 2, 3];
+    println!("print with a debugging formatter: {:?}", ar);
 }
 
 /**
@@ -77,8 +115,10 @@ fn play_variable_stuff() {
     println!("Number is {}", x);
     x = 6;
     println!("Number is {}", x);
-    
+
     // Constants are always immutable and a type has to be specified.
+    // Rust’s naming convention for constants is to use all uppercase with underscores between
+    // words
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("Constant is {}", THREE_HOURS_IN_SECONDS);
 
@@ -87,9 +127,40 @@ fn play_variable_stuff() {
     println!("s is {}", s);
     {
         let s = s * 2;
-        println!("s inside a block is {}", s);
+        println!("s inside a block is {}. It's shadowing", s);
+        let s = "abcde";
+        println!("Shadowing can change the type. s inside a block is {}.", s);
     }
     println!("s outside the block is still {}", s);
+}
+
+/**
+ * Play some data types.
+ */
+fn play_data_types() {
+    let dec = 1.123;
+    let byte = b'A';
+    let c = 'A';
+
+    println!("dec = {0}, byte = {1}, c = {2}", dec, byte, c);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (a, b, c) = tup;
+    println!("first = {}, b = {}, last = {}", tup.0, b, tup.2);
+    println!("a = {}, c = {}", a, c);
+
+    // array has fixed length, and all elements have to ahve the same type.
+    // An array is allocated on stack.
+    let ar = [1, 2, 3];
+
+    // Array of i32, 5 elements
+    let ar_with_type: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("ar: {:?}, first value: {}", ar, ar[0]);
+    println!("ar_with_type: {:?}", ar_with_type);
+
+    // an array with initial value of 3, with 5 elements
+    let ar_with_initial_value = [3; 5];
+    println!("ar_with_initial_value: {:?}", ar_with_initial_value);
 }
 
 /**
